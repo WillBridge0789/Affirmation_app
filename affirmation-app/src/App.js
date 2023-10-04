a285e3b95e3529991d612af41c99ec069fe9837c
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-10-04 17:56:25 +0000

    Home component added

---
+++ affirmation-app/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './components/Home';
+import './css/styles.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
