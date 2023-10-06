88232bdcebfc0d3f8648dbe77615fe9994405b60
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-10-06 19:48:49 +0000

    Got affirmations to show randomly upon page render

--- affirmation-app/src/App.js
+++ affirmation-app/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './components/Home';
+import Affirm from './components/Affirm';
 import './css/styles.css';
 
 function App() {
   return (
     <>
       <Home />
+      <Affirm />
     </>
   );
 }
--- affirmation-app/src/components/Affirm.jsx
+++ affirmation-app/src/components/Affirm.jsx
@@ -1,22 +1,52 @@
-function Affirm() {
-    fetch("https://type.fit/api/quotes")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+const { useState, useEffect } = require("react");
 
-    return(
-        <div className="container">
-            <div className="row d-flex justify-content-center">
-                <div className="col d-flex align-items-center">
-                    <h1>For Today:</h1>
-                </div>
-                <div className="col d-flex align-items-center">
-                    <h2>{}</h2>
-                </div>
-            </div>
-        </div>
-    )
+function shuffleAffirmations(array) {
+  const shuffledAffirmations = [...array];
+  for (let i = shuffledAffirmations.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledAffirmations[i], shuffledAffirmations[j]] = [
+      shuffledAffirmations[j],
+      shuffledAffirmations[i],
+    ];
+  }
+  return shuffledAffirmations;
 }
+
+function Affirm() {
+  const [affirmations, setAffirmations] = useState([]);
+  const [currentAffirmationIndex, setCurrentAffirmationIndex] = useState(0);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const shuffleData = shuffleAffirmations(data);
+        setAffirmations(shuffleData);
+      });
+  }, []);
+
+  const nextAffirmation = () => {
+    setCurrentAffirmationIndex(
+      (prevIndex) => (prevIndex + 1) % affirmations.length
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center m-3">
+        <h1 className="d-flex justify-content-center">For Today:</h1>
+      </div>
+      <div className="row d-flex justify-content-center m-3">
+        {/* {affirmations.map((affirmation, index) => (
+                <h2 key={index}>{affirmation.text}</h2>
+              ))} */}
+        <p className="d-flex justify-content-center">{affirmations[currentAffirmationIndex]?.text}</p>
+        <button onClick={nextAffirmation}>Next Affirmation</button>
+      </div>
+    </div>
+  );
+}
+
+export default Affirm;
